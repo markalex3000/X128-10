4c467fa57fea047b30c516481473fad98ab0c36d
Mark.Alexieff <Mark.Alexieff@INA-8PMPKX1>
2016-12-23 15:04:30 -0700

    Working code

--- X128-10/Source.cpp
+++ X128-10/Source.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <random>
 
 using namespace std;
 
@@ -15,4 +16,93 @@ inline void simple_error(string s)	// write ``error: s and exit program
 	cerr << "error: " << s << '\n';
 	keep_window_open();		// for some Windows environments
 	exit(1);
+}
+
+int main() {
+	int user_score{ 0 }, comp_score{ 0 };
+	char user_move{ 'x' };
+	vector<char> comp_move = { 'r', 'p', 's' };
+	int comp_move_idx{ 0 };
+
+	while (true) {
+		cout << "Enter your move (r)ock, (p)aper, (s)cisssors.  Enter x to exit: ";
+		cin >> user_move;
+		// check input
+		switch (user_move)
+		{
+			case 'r': case 'p': case 's':
+				break;
+			case 'x': {
+				cout << "Your score: " << user_score << "  Computer score: " << comp_score << "\n\n";
+				keep_window_open();
+				return (0);
+			}
+			default: //bad input
+				continue;
+		}
+
+		// generate computer move
+		random_device rd;   // non-deterministic generator  
+		mt19937 gen(rd());  // to seed mersenne twister.  
+		uniform_int_distribution<> dist(1, 3); // distribute results between 1 and 3 inclusive.  
+
+		comp_move_idx = dist(gen)-1;
+
+		// evaluate
+
+		switch (user_move)
+		{
+		case 'r':
+			if (comp_move[comp_move_idx] == 'r') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: Draw\n\n";
+				continue;
+			}
+			else if(comp_move[comp_move_idx] == 'p') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: Computer Wins\n\n";
+				comp_score++;
+				continue;
+			}
+			else if (comp_move[comp_move_idx] == 's') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: You win!\n\n";
+				user_score++;
+				continue;
+			}
+			break;
+		case 'p':
+			if (comp_move[comp_move_idx] == 'r') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: You win!\n\n";
+				user_score++;
+				continue;
+			}
+			else if (comp_move[comp_move_idx] == 'p') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: Draw\n\n";
+				continue;
+			}
+			else if (comp_move[comp_move_idx] == 's') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: Computer wins.\n\n";
+				comp_score++;
+				continue;
+			}
+			break;
+		case 's':
+			if (comp_move[comp_move_idx] == 'r') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: Computer wins.\n\n";
+				comp_score++;
+				continue;
+			}
+			else if (comp_move[comp_move_idx] == 'p') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: You win!\n\n";
+				user_score++;
+				continue;
+			}
+			else if (comp_move[comp_move_idx] == 's') {
+				cout << "Your move: " << user_move << "\tComputer move: " << comp_move[comp_move_idx] << "\tResult: Draw.\n\n";
+				continue;
+			}
+			break;
+		default:
+			cout << "default case reached for some reason... \n";
+			break;
+		}
+	}
 }
